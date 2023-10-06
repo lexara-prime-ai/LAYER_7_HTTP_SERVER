57325984b8367d7a5663964683cbdb7c12a7871e
projectfinalaudio <irfanghat@gmail.com>
2023-10-06 14:41:56 +0300

    Update: Added static file handling

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,5 +1,6 @@
 use crate::http::{Request, Response, StatusCode, RequestMethod};
 use super::server::Handler;
+use std::fs;
 
 pub struct WebsiteHandler {
     public_path: String,
@@ -10,14 +11,20 @@ impl WebsiteHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+
+    // Method for serving static files
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        fs::read_to_string(path).ok()
+    }
 }
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             RequestMethod::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h2>Http Server</h2>".to_string())),
-                "/downloads" => Response::new(StatusCode::Ok, Some("<h2>Http Server 1.1 -> :: Rust v1.72,0</h2><a href=\"www.google.com\">Downloads</a></h2>".to_string())),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/downloads" => Response::new(StatusCode::Ok, self.read_file("downloads.html")),
                 _ => Response::new(StatusCode::NotFound, None),
             },
             _ => Response::new(StatusCode::NotFound, None),
